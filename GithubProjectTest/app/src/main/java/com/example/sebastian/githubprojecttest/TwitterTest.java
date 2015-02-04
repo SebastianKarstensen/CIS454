0a11473b1c8637d567e26fda16787c0a6ed1e9a8
Sebastian Læsøe Karstensen <slae@itu.dk>
2015-02-03 20:57:51 -0500

    Added conversion method from tweet to URL-string

--- GithubProjectTest/app/src/main/java/com/example/sebastian/githubprojecttest/TwitterTest.java
+++ GithubProjectTest/app/src/main/java/com/example/sebastian/githubprojecttest/TwitterTest.java
@@ -1,24 +1,41 @@
 package com.example.sebastian.githubprojecttest;
 
+import com.example.sebastian.githubprojecttest.util.SystemUiHider;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by Sebastian on 03-02-2015.
  */
 public class TwitterTest {
 
+    public String convertToURL(String tweet) {
+        String encode = "";
+        try {
+            encode = java.net.URLEncoder.encode(tweet, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return encode.replace("+","%20");
+    }
+
     public void Tweet() {
 
-        String message = "This is a test";
-        String tweet = message.replace(" ", "%20");
+        SystemUiHider.OnVisibilityChangeListener
+
+        String tweet = "This is a test";
+
+        String message = convertToURL(tweet);
 
         HttpPost httpPost = new HttpPost(
-                "http://api.twitter.com/1.1/statuses/update.json?status=" + tweet);
+                "http://api.twitter.com/1.1/statuses/update.json?status=" + message);
 
         oAuthConsumer.sign(httpPost);
 
