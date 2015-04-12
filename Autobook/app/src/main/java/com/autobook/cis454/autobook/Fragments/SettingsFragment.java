5c894457b9c174ad83083d436bc34df620254af7
Anders <anderskroyer@yahoo.dk>
2015-04-12 12:49:09 -0400

    scheduler now creates a notification when the user is not logged onto either twitter or facebook

--- Autobook/app/src/main/java/com/autobook/cis454/autobook/Fragments/SettingsFragment.java
+++ Autobook/app/src/main/java/com/autobook/cis454/autobook/Fragments/SettingsFragment.java
@@ -1,8 +1,13 @@
 package com.autobook.cis454.autobook.Fragments;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NotificationCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +17,7 @@ import android.widget.Toast;
 import com.autobook.cis454.autobook.Activities.HomeActivity;
 import com.autobook.cis454.autobook.Helpers.TwitterHelper;
 import com.autobook.cis454.autobook.R;
+import com.autobook.cis454.autobook.Scheduler.AlarmManagerBroadcastReceiver;
 
 import java.util.Date;
 import java.util.concurrent.ExecutionException;
@@ -92,6 +98,25 @@ public class SettingsFragment extends Fragment {
             }
         });
 
+        buttonCredits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NotificationCompat.Builder mBuilder =
+                        new NotificationCompat.Builder(getActivity())
+                                .setSmallIcon(R.drawable.autobook_logo_v01)
+                                .setContentTitle("Failed to execute the event")
+                                .setContentText("Event title: ");
+
+                // Sets an ID for the notification
+                int mNotificationId = 001;
+                // Gets an instance of the NotificationManager service
+                NotificationManager mNotifyMgr =
+                        (NotificationManager) getActivity().getSystemService(getActivity().NOTIFICATION_SERVICE);
+                // Builds the notification and issues it.
+                mNotifyMgr.notify(mNotificationId, mBuilder.build());
+
+            }
+        });
         updateFragment();
 
         return rootView;
