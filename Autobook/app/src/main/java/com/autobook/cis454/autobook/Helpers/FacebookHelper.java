bb7ee7669c34f115a5afaa58a2fda9058c3b94e6
Kenton Guan <kenton.guan@gmail.com>
2015-04-14 10:30:12 -0400

    added post to wall method to FacebookHelper

--- Autobook/app/src/main/java/com/autobook/cis454/autobook/Helpers/FacebookHelper.java
+++ Autobook/app/src/main/java/com/autobook/cis454/autobook/Helpers/FacebookHelper.java
@@ -1,10 +1,17 @@
 package com.autobook.cis454.autobook.Helpers;
 
 import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.google.gson.Gson;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 /**
  * Created by Kenton on 4/12/2015.
@@ -12,12 +19,36 @@ import android.content.SharedPreferences.Editor;
 public class FacebookHelper
 {
     SharedPreferences mSharedPreferences;
+    AccessToken accessToken = AccessToken.getCurrentAccessToken();
 
     public boolean isFacebookLoggedIn()
     {
-        Gson gson = new Gson();
-        String json = mSharedPreferences.getString("fb_access_token","");
-        AccessToken accessToken = gson.fromJson(json, AccessToken.class);
         return accessToken.isExpired();
+    }
+
+    public void postToFBWall(String message)
+    {
+        GraphRequest.GraphJSONObjectCallback callback = new GraphRequest.GraphJSONObjectCallback()
+        {
+            @Override
+            public void onCompleted(JSONObject object,GraphResponse response)
+            {
+                Toast.makeText(Autobook.getAppContext(), response.getRawResponse(), Toast.LENGTH_LONG).show();
+            }
+        };
+
+        Bundle params = new Bundle();
+        params.putString("message", message);
+        new GraphRequest(
+                accessToken,
+                "/me/feed",
+                params,
+                HttpMethod.POST,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        Toast.makeText(Autobook.getAppContext(),response.getRawResponse(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        ).executeAsync();
     }
 }
