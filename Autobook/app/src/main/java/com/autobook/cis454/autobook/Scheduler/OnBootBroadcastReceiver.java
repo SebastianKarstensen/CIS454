452bb49f4b931574cdb42abf34afa0fb54dc33ba
Anders <anderskroyer@yahoo.dk>
2015-04-26 12:36:41 -0400

    Deleted unused imports in onBootReceiver

--- Autobook/app/src/main/java/com/autobook/cis454/autobook/Scheduler/OnBootBroadcastReceiver.java
+++ Autobook/app/src/main/java/com/autobook/cis454/autobook/Scheduler/OnBootBroadcastReceiver.java
@@ -1,14 +1,10 @@
 package com.autobook.cis454.autobook.Scheduler;
 
-import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
-
 import com.autobook.cis454.autobook.Event.Event;
 import com.autobook.cis454.autobook.Helpers.Storage;
-import com.autobook.cis454.autobook.R;
 
 import java.util.ArrayList;
 
